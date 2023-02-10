adb0654118646973ac3c25519f3304cd65d59dc7
66wasnthere <90938274+66wasnthere@users.noreply.github.com>
2023-02-09 20:36:31 -0600

    Update Alt_Control.lua

--- Alt_Control.lua
+++ Alt_Control.lua
@@ -1,1 +1,13 @@
-print("HACKER")
+getgenv().Settings = {
+    host = 123456789,
+    prefix = '/',
+    fps = 3,
+    admsg = 'free'
+}
+
+getgenv().Alts = {
+    123456789,
+    123456789,
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/66wasnthere/ok/main/code.lua"))()
